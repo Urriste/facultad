bb971587264cf66de0fe12bcd06524aa3d12f1a9
Lucas Urriste <lukeu2002@gmail.com>
2021-09-09 22:42:46 -0300

    new exercises added

---
+++ Taller de programacion/Practicas/Ejercicios/Practica 1/pRecursionEjer2.pas
@@ -0,0 +1,76 @@
+program pRecursionEjer2;
+const
+	dimF = 5;
+type 	
+
+	vector = array [1.. dimF] of integer;
+	
+procedure leerValor(var num:integer);
+begin
+
+	writeln('Ingrese un numero');
+	readln(num);
+
+end;
+	
+procedure cargarV(var v:vector);
+var
+	i,num:integer;
+	
+begin
+
+	for i:= 1 to dimF do begin
+		leerValor(num);
+		v[i]:= num;
+	end;
+
+end;
+
+function maximo(v:vector;i:integer):integer;
+var
+	max:integer;
+begin
+	
+	if(i < dimF) then begin
+		max:= maximo(v,i+1);
+		if(v[i]> max) then begin	
+			maximo:= v[i];
+		end
+		else begin
+			maximo:= max;
+		end;
+	end
+	else begin
+		maximo:= -9999;
+	end;
+end;
+
+function sumarVector(v:vector;i:integer):integer;
+begin
+
+	if(i <= dimF) then begin
+		sumarVector:= v[i] + sumarVector(v,i+1);
+	end
+	else begin
+		sumarVector:= 0;
+	end;
+end;
+	
+var
+	v:vector;
+	valMax,sumaTotal,i:integer;
+	
+BEGIN
+	i:=1;
+	sumaTotal:= 0;
+	cargarV(v);
+	
+	valMax:= maximo(v,i);
+	
+	writeln('Max: ', valMax);	
+	
+	sumaTotal:= sumarVector(v,i);
+	writeln('Suma total: ', sumaTotal);
+	
+END.
+
